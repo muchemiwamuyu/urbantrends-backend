b6c997c0bdcd48cecf500a46306f954315bf0ec9
muchemi wamuyu <muchemiedwin68@gmail.com>
2025-12-05 09:12:59 +0300

    more endpoints

--- src/routes/services/serviceRoutes.js
+++ src/routes/services/serviceRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { addService, getServices } from "../../services/service.js";
+import { addService, deleteService, getServices } from "../../services/service.js";
 
 const router = express.Router()
 
 // relevant routes
 router.post('/create', addService)
-router.get('/sers', getServices)
+router.get('/sers', getServices);
+router.delete('/service/:id', deleteService);
 
 export default router
